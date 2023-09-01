52b808cb4d9c77129403762fa7a409ef27d61213
Mukesh216 <727721euec095@skcet.ac.in>
2023-09-01 09:06:29 +0530

    first

---
+++ src/main/java/com/example/demo/service/TransactionHistoryService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.modal.TransactionHistory;
+import com.example.demo.repo.TransactionHistoryRepo;
+
+@Service
+public class TransactionHistoryService {
+	
+	@Autowired
+	TransactionHistoryRepo thr;
+	
+	public List<TransactionHistory> getTransactionHistory() {
+		return thr.findAll();
+	}
+	
+	public void addTransactionHistory(TransactionHistory th) {
+		thr.save(th);
+	}
+	
+}
